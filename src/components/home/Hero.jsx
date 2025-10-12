d63dd56b677d27a91b9ce47e2afb32f3baaa963e
ayoubMO19 <ayoubweb04@gmail.com>
2025-10-12 17:11:04 +0200

    cambiando la home para incluir login y registre directamente sin mantenerlo separado

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,10 +3,19 @@ import styles from './Hero.module.css';
 const Hero = () => {
     return (
         <div className={styles.container}>
-            <h1 className={styles.title} >VEXA DASHBOARD</h1>
-            <div className={styles.buttonsCointainer}>
-                <button>Login</button>
-                <button>Register</button>
+            <div className ={styles.subContainer}>
+                <div className={styles.titleContainer}>
+                    <h1>DASHBOARD ACCESS</h1>
+                </div>
+                <div className={styles.accessContainer}>
+                    <h1>Welcome to VEXA</h1>
+                    <input placeholder='Username' type="text"></input>
+                    <input placeholder='Password' type='password'></input>
+                    <div>
+                        <span>Don't have an account ? <a>Sign Up</a></span>
+                        <button>Login</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
