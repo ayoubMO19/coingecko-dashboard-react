2626d4ccb22dcde8a09b624490aab9ad83ea37a7
ayoubMO19 <ayoubweb04@gmail.com>
2025-10-13 16:59:06 +0200

    Se ha terminado de desarollar la interfaz login

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -5,14 +5,28 @@ const Hero = () => {
         <div className={styles.container}>
             <div className ={styles.subContainer}>
                 <div className={styles.titleContainer}>
-                    <h1>DASHBOARD ACCESS</h1>
+                    <h1>Welcome to <strong>VEXA</strong></h1>
+                    <h2>Dashboard access</h2>
+                    <p>Accede a un dashboard exclusivo con información en tiempo real del mercado de criptomonedas. Consulta porcentajes de variación, gráficos interactivos y datos clave que te permitirán seguir las tendencias más importantes del sector. 
+                    <br/><br/> 
+                    Descubre análisis detallados y herramientas que te ayudarán a interpretar el comportamiento del mercado de forma más precisa. ¡Ingresa ahora y aprovecha todo el potencial que nuestro panel pone a tu alcance!</p>
                 </div>
                 <div className={styles.accessContainer}>
-                    <h1>Welcome to VEXA</h1>
-                    <input placeholder='Username' type="text"></input>
-                    <input placeholder='Password' type='password'></input>
-                    <div>
-                        <span>Don't have an account ? <a>Sign Up</a></span>
+                    <div className={styles.accessTitle}>
+                        <h1>User Login</h1>
+                    </div>
+                    <div className={styles.accessInputs}>
+                        <div className={styles.accessInput}>
+                            <span>Username</span>
+                            <input placeholder='Input your Username' type="text"></input>
+                        </div>
+                        <div className={styles.accessInput}>
+                            <span>Password</span>
+                            <input placeholder='Input your Password' type='password'></input>
+                        </div>
+                    </div>
+                    <div className={styles.accessButton}>
+                        <span>Don't have an account? <a href='https://www.youtube.com/'>Sign Up</a></span>
                         <button>Login</button>
                     </div>
                 </div>
