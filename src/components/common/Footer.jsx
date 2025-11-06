4162746683029c17fe35304411c15cfd08376803
ayoubMO19 <ayoubweb04@gmail.com>
2025-11-06 15:54:10 +0100

    limpieza de imagenes que no se usan y ajustes de las que se usan

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,15 +1,16 @@
 import styles from './Footer.module.css';
-
+import githubLogo from '../../assets/github-logo.png';
+import linkedinLogo from '../../assets/linkedin-logo.png';
 const Footer = () => {
   return (
     <footer className={styles.container}>
       <a href="https://github.com/ayoubMO19" target="_blank" rel="noopener noreferrer" className={styles.link}>
-        <img src="/github-logo.png" alt="GitHub" />
+        <img src={githubLogo} alt="GitHub" />
         <span>AyoubMO19</span>
       </a>
 
       <a href="https://www.linkedin.com/in/ayoub-morghi-ouhda/" target="_blank" rel="noopener noreferrer" className={styles.link}>
-        <img src="/linkedin-logo.png" alt="LinkedIn" />
+        <img src={linkedinLogo} alt="LinkedIn" />
         <span>Ayoub Morghi</span>
       </a>
     </footer>
